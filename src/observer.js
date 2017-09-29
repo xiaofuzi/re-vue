05dad5d7b92458ba1e5b02763f0669ac2d6513cc
杨小福 <yangxiaofuPL@163.com>
2017-09-28 21:36:08 -0500

    Revert "chore: tiny-vue找不到isShow变量更正，其他一些工程项目配置更新"

--- src/observer.js
+++ src/observer.js
@@ -5,34 +5,29 @@ let observer = new Event();
 /**
  * Array observer
  */
-let arrProto = Array.prototype;
-let arrayMethods = Object.create(arrProto);
-const mutationMethods = [
-    'pop',
-    'push',
-    'reverse',
-    'shift',
-    'unshift',
-    'splice',
-    'sort'
-];
+let arrProto = Array.prototype,
+    mutationMethods = [
+        'pop',
+        'push',
+        'reverse',
+        'shift',
+        'unshift',
+        'splice',
+        'sort'
+    ];
 
-function arrayObserver (arr, cb) {
+function arrayObserver(arr, cb) {
     mutationMethods.forEach(function (method) {
-        // 原生的数组方法
-        let originalMethod = arrProto[method];
+        arr[method] = function (...rest) {
+            arrProto[method].apply(this, rest);
 
-        arrayMethods[method] = function () {
-            originalMethod.apply(this, arguments);
-
-            cb && cb({
+            cb&&cb({
                 event: method,
-                args: arguments,
+                args: rest,
                 array: arr
             });
         };
     });
-    arr.__proto__ = arrayMethods;
 }
 
 export {
